b7cf8e768d576f6c95b9879fad4c15801bb0aced
Billy <billyurrutia@freebirdstores.com>
2021-06-23 16:16:56 -0600

    Service test for Movie db call

---
+++ spec/services/moviedb_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Movie Db Service',:vcr do 
+  it "should return popular movie list" do 
+    movies = MovieDbService.get_results('popular')
+
+    expect(movie).to have_key(:data)
+    expect(movie[:data].first).to have_key(:original_title)
+    expect(movie[:data].first).to have_key(:overview)
+    expect(movie[:data].first).to have_key(:adult)
+    expect(movie[:data].first).to have_key(:popularity)
+    expect(movie[:data].first).to have_key(:poster_path)
+    expect(movie[:data].first).to have_key(:release_date)
+    expect(movie[:data].first).to have_key(:title)
+    expect(movie[:data].first).to have_key(:vote_average)
+    expect(movie[:data].first).to have_key(:vote_count)
+    expect(movie[:data].first).to have_key(:genre_ids)
+  end
+end
