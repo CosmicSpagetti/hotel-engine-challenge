b387641b5ff51d1f6e05ea25f93d7413012a8a13
Billy <billyurrutia@freebirdstores.com>
2021-06-24 15:15:00 -0600

    do i wanna just make a random token?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_secure_password 
     validates :email, uniqueness: true, presence: true
     validates_presence_of :password, require: true
+
+    def token 
+      
+    end
 end
