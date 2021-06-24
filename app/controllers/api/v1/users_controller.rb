4e93fd86a3dbf29d2fe8ce5b8c5302a465d15aa7
Billy <billyurrutia@freebirdstores.com>
2021-06-24 15:12:09 -0600

    Built out controller for users thinking maybe use jtw

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    
+    user = User.new(user_params)
+    if user.save
+      render json: { api_key: user.token }, status: 201 
+    else 
+      render json: {error: "Unable to create User"}, status: 409
+    end
+  end
+
+  private 
+
+  def user_params 
+    params.permit(:email, :password, :password_confirmation)
   end
 end
