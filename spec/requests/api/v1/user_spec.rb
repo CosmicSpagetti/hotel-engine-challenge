60aa9f54995f67315c9310450dbf95228b0816a5
Billy <billyurrutia@freebirdstores.com>
2021-06-24 17:41:15 -0600

    keep updating tests. Thanks job with no testing I forgot everything

--- spec/requests/api/v1/user_spec.rb
+++ spec/requests/api/v1/user_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
 describe 'User endpoint' do 
+  before :each do 
+    User.destroy_all
+  end
+
   it 'should create a user when email,password, and password confirmation is sent' do 
     params = {"email": "billy@example.com","password": "password123", 'password_confirmation': 'password123'}
     headers = { 'Content-Type': 'application/json', 'Accept': 'application/json'}
 
     post '/api/v1/users', params: params.to_json, headers: headers
 
-    user = User.find_by(email: params['email'])
+    user = User.find_by(email: params[:email])
 
     expect(user.email).to eq('billy@example.com') 
+
     expect(JSON.parse(response.body)).to have_key('token')
     expect(JSON.parse(response.body)['token']).to be_a String
   end
 
   it 'should log user in and respond with their token' do 
-    params = {"email": "billy@example.com","password": "password123" }
+    params = {"email": "billy23@example.com","password": "password123" }
     User.create!(params)
     headers = {'Content-Type': 'application/json','Accept': 'application/json'}
 
@@ -26,7 +31,7 @@ describe 'User endpoint' do
   end
 
   it 'should send error when wrong password is sent' do 
-    params = {"email": "billy@example.com", "password": "bloop"}
+    params = {"email": "billy34@example.com", "password": "bloop"}
     User.create!( params)
 
 
