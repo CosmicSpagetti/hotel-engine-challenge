b92c4b9c8e2c6160182dab59ec701888ee2c697f
Billy <billyurrutia@freebirdstores.com>
2021-06-23 22:11:53 -0600

    add new test for popular movies and for now playing movies

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 describe 'Movie Db Service',:vcr do 
+  it "should return now playing movie list" do 
+    movies = MovieService.get_now_playing_movies
+
+    expect(movies).to have_key(:results)
+    expect(movies[:results].first).to have_key(:original_title)
+    expect(movies[:results].first).to have_key(:overview)
+    expect(movies[:results].first).to have_key(:adult)
+    expect(movies[:results].first).to have_key(:popularity)
+    expect(movies[:results].first).to have_key(:poster_path)
+    expect(movies[:results].first).to have_key(:release_date)
+    expect(movies[:results].first).to have_key(:title)
+    expect(movies[:results].first).to have_key(:vote_average)
+    expect(movies[:results].first).to have_key(:vote_count)
+    expect(movies[:results].first).to have_key(:genre_ids)
+  end
+
   it "should return popular movie list" do 
     movies = MovieService.get_popular_movies
 
