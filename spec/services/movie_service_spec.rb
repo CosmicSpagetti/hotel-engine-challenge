d139071166fd06545aeef7acdab575d5525162ce
Billy <billyurrutia@freebirdstores.com>
2021-06-24 08:31:22 -0600

    Made a new test and then started wondering how these would be any different from one another.

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -32,4 +32,20 @@ describe 'Movie Db Service',:vcr do
     expect(movies[:results].first).to have_key(:vote_count)
     expect(movies[:results].first).to have_key(:genre_ids)
   end
+
+  it "should return upcoming movie list" do 
+    movies = MovieService.get_upcoming_movies
+
+    expect(movies).to have_key(:results)
+    expect(movies[:results].first).to have_key(:original_title)
+    expect(movies[:results].first).to have_key(:overview)
+    expect(movies[:results].first).to have_key(:adult)
+    expect(movies[:results].first).to have_key(:popularity)
+    expect(movies[:results].first).to have_key(:poster_path)
+    expect(movies[:results].first).to have_key(:release_date)
+    expect(movies[:results].first).to have_key(:title)
+    expect(movies[:results].first).to have_key(:vote_average)
+    expect(movies[:results].first).to have_key(:vote_count)
+    expect(movies[:results].first).to have_key(:genre_ids)
+  end
 end
