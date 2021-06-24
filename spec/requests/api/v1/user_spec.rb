d999199e0322ff93ee2d2e4715151f5a5e06783e
Billy <billyurrutia@freebirdstores.com>
2021-06-24 15:11:34 -0600

    can't send ruby hash with content-type: json

--- spec/requests/api/v1/user_spec.rb
+++ spec/requests/api/v1/user_spec.rb
@@ -2,21 +2,18 @@ require 'rails_helper'
 
 describe 'User endpoint' do 
   it 'should create a user when email,password, and password confirmation is sent' do 
-    params = {"email": "billy@example.com","password": "password123" }
-
+    params = {"email": "billy@example.com","password": "password123", 'password_confirmation': 'password123'}
     headers = { 'Content-Type': 'application/json', 'Accept': 'application/json'}
 
-    post '/api/v1/users', params: params, headers: headers
+    post '/api/v1/users', params: params.to_json, headers: headers
   end
 
   it 'should log user in and respond with their token' do 
     params = {"email": "billy@example.com","password": "password123" }
-
     User.create!(params)
-
     headers = {'Content-Type': 'application/json','Accept': 'application/json'}
 
-    post '/api/v1/sessions', params: params, headers: headers
+    post '/api/v1/sessions', params: params.to_json, headers: headers
 
     expect(JSON.parse(response.body)).to have_key('token')
     expect(JSON.parse(response.body)['token']).to be_a String
@@ -29,7 +26,7 @@ describe 'User endpoint' do
 
     headers = {'Content-Type': 'application/json','Accept': 'application/json'}
 
-    post '/api/v1/sessions', params: params, headers: headers
+    post '/api/v1/sessions', params: params.to_json, headers: headers
 
     expect(JSON.parse(response.body)).to have_key('error')
     expect(JSON.parse(response.body)['error']).to eq('Unable to authenticate user')
