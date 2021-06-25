6b7c07c1e52fb8c1b8e73a761eae9cf547b3f67a
Billy <billyurrutia@freebirdstores.com>
2021-06-25 15:39:38 -0600

    fix broken test

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Movie Db Service',:vcr do 
   it "should return now playing movie list" do 
-    movies = MovieService.get_now_playing_movies
+    movies = MovieService.get_list('now_playing')
     # should create a mock instead of having 3 tests that look the same :/
     expect(movies).to have_key(:results)
     expect(movies[:results].first).to have_key(:original_title)
@@ -18,7 +18,7 @@ describe 'Movie Db Service',:vcr do
   end
 
   it "should return popular movie list" do 
-    movies = MovieService.get_popular_movies
+    movies = MovieService.get_list('popular')
     
     expect(movies).to have_key(:results)
     expect(movies[:results].first).to have_key(:original_title)
@@ -33,21 +33,6 @@ describe 'Movie Db Service',:vcr do
     expect(movies[:results].first).to have_key(:genre_ids)
   end
 
-  it "should return upcoming movie list" do 
-    movies = MovieService.get_upcoming_movies
-
-    expect(movies).to have_key(:results)
-    expect(movies[:results].first).to have_key(:original_title)
-    expect(movies[:results].first).to have_key(:overview)
-    expect(movies[:results].first).to have_key(:adult)
-    expect(movies[:results].first).to have_key(:popularity)
-    expect(movies[:results].first).to have_key(:poster_path)
-    expect(movies[:results].first).to have_key(:release_date)
-    expect(movies[:results].first).to have_key(:title)
-    expect(movies[:results].first).to have_key(:vote_average)
-    expect(movies[:results].first).to have_key(:vote_count)
-    expect(movies[:results].first).to have_key(:genre_ids)
-  end
 
   it 'should return movie you are looking for' do 
     movies = MovieService.search_movies({'query'=> 'matrix'})
@@ -61,12 +46,12 @@ describe 'Movie Db Service',:vcr do
   end
 
   it 'should be able to paginate with page param added' do 
-    movies = MovieService.get_upcoming_movies 
+    movies = MovieService.get_list('upcoming') 
 
     expect(movies).to have_key(:page)
     expect(movies[:page]).to eq(1)
 
-    movies_2 = MovieService.get_upcoming_movies({'page'=> 2})
+    movies_2 = MovieService.get_list('upcoming',{'page'=> 2})
     expect(movies_2).to have_key(:page)
     expect(movies_2[:page]).to eq(2)
   end
