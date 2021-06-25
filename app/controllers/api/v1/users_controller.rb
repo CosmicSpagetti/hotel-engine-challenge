ec26a60878f8a8ff069996aaf793840cb5b7d69f
Billy <billyurrutia@freebirdstores.com>
2021-06-24 18:28:34 -0600

    update create controller to return the token. Is this too many ifs?

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::UsersController < ApplicationController
-  def create
+  skip_before_action :authenticate_request
+
+   def create
     user = User.new(user_params)
     if user.save
-      render json: { api_key: user.token }, status: 201 
+      command = AuthenticateUser.call(user.email, user.password)
+      if command.success?
+        render json: { token: command.result }, status: 201 
+      else
+        render json: { error: command.errors }, status: :unauthorized
+      end
     else 
-      render json: {error: "Unable to create User"}, status: 409
+      render json: {error: "Unable to create User"}, status: :unauthorized
     end
   end
 
